fd6b85a2528733fb67f90316acfd7aeb087206ee
Aniket Vairagade <124672101+aniketv-orderat@users.noreply.github.com>
2023-07-27 16:54:52 +0530

    QR-Code-Generator

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const wrapper = document.querySelector(".wrapper"),
 qrInput = wrapper.querySelector(".form input"),
 generateBtn = wrapper.querySelector(".form button"),
 qrImg = wrapper.querySelector(".qr-code img");
+ downloadBtn = wrapper.querySelector(".qr-code button"); // download btn code start
+
 
 
 generateBtn.addEventListener("click", () => {
+    console.log("genrating QR Code ");
+
     let qrValue = qrInput.value;
     if(!qrValue) return; // if the input is empty then return form here
     generateBtn.innerText = "Generating QR Code...";
@@ -15,7 +19,53 @@ generateBtn.addEventListener("click", () => {
         wrapper.classList.add("active");
         generateBtn.innerText = "Generating QR Code";
     });
+    console.log("genrating QR Code sucessfully");
 });
+
+
+// download btn code start
+downloadBtn.addEventListener("click", () => {
+    console.log("download QR Code called ");
+    let img = document.querySelector(".qr-code img");
+
+    if(img !== null){
+        let imgAtrr = img.getAttribute("src"); 
+        console.log(imgAtrr);
+        downloadImage(imgAtrr);
+        
+        // downloadBtn.getElementById("downloadBtns", imgAtrr);
+        // document.getElementById('downloadBtn').src = imgAtrr;
+    }
+    else{
+        downloadBtn.setAttribute("href", `${document.querySelector("canvas").toDataURL}`);
+
+    }
+});
+
+
+async function downloadImage(
+    imageSrc,
+    nameOfDownload = 'Qr_Code.png',
+  ) {
+    const response = await fetch(imageSrc);
+  
+    const blobImage = await response.blob();
+  
+    const href = URL.createObjectURL(blobImage);
+  
+    const anchorElement = document.createElement('a');
+    anchorElement.href = href;
+    anchorElement.download = nameOfDownload;
+  
+    document.body.appendChild(anchorElement);
+    anchorElement.click();
+  
+    document.body.removeChild(anchorElement);
+    window.URL.revokeObjectURL(href);
+  }
+
+// download btn code end
+
 
 qrInput.addEventListener("keyup", () => {
     if(!qrValue.value) {
